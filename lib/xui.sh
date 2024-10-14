d3460034428ec91d1031416379169b81c4930bf0
damshenas <19164817+damshenas@users.noreply.github.com>
2024-10-14 23:55:09 +0200

    fixed version

--- lib/xui.sh
+++ lib/xui.sh
@@ -2,6 +2,7 @@
 
 export cert_dir="/opt/certs"
 export arch="amd64"
+export xui_version="v2.4.4"
 
 mkdir -p $cert_dir
 cp $s3_dir/sib.ftp.sh.cer $cert_dir/
@@ -12,9 +13,8 @@ cp $scripts_dir/lib/xui.service /etc/systemd/system/x-ui.service
 mkdir -p /etc/x-ui
 cp $s3_dir/x-ui.db /etc/x-ui/x-ui.db
 
-latest_tag_version=$(curl -Ls "https://api.github.com/repos/MHSanaei/3x-ui/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 cd /opt
-wget -N --no-check-certificate -O x-ui-linux-${arch}.tar.gz https://github.com/MHSanaei/3x-ui/releases/download/${latest_tag_version}/x-ui-linux-${arch}.tar.gz
+wget -N --no-check-certificate -O x-ui-linux-${arch}.tar.gz https://github.com/MHSanaei/3x-ui/releases/download/${xui_version}/x-ui-linux-${arch}.tar.gz
 tar zxvf x-ui-linux-${arch}.tar.gz
 rm x-ui-linux-${arch}.tar.gz -f
 cd x-ui
