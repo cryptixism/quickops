e406612f754404762cfdbe841751e9f522a24778
damshenas <19164817+damshenas@users.noreply.github.com>
2024-10-14 23:23:01 +0200

    fix mtg

--- lib/mtg.sh
+++ lib/mtg.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 
 cp $s3_dir/mtg /usr/local/bin/mtg
-cp $scripts_dir/mtg.service /etc/systemd/system/mtg.service
-cp $scripts_dir/mtg.toml /etc/mtg.toml
+cp $scripts_dir/lib/mtg.service /etc/systemd/system/mtg.service
+cp $scripts_dir/lib/mtg.toml /etc/mtg.toml
 
 sed -i "s|SECRET_ENV|${mtg_secret}|g" /etc/mtg.toml
 sed -i "s|BIND_PORT_ENV|${mtg_port}|g" /etc/mtg.toml
