6354bae984c852cd560abeeadfa9fa7a504d1709
damshenas <19164817+damshenas@users.noreply.github.com>
2024-10-14 22:44:32 +0200

    bugfix

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,17 +5,18 @@ set -x
 export s3_dir=/opt/s3
 export s3_bucket_name=${s3_bucket_arn##*:}
 aws s3 sync s3://${s3_bucket_name}/data $s3_dir/
+chmod +x -R $s3_dir/
 
 ## handle address updates
-sudo lib/address.sh
+sudo ./lib/address.sh
 
 ## installing and configuring xui and xray
 ## https://github.com/MHSanaei/3x-ui
-sudo lib/xui.sh
+sudo ./lib/xui.sh
 
 ## installing and configuring mtg
 ## https://github.com/9seconds/mtg
-sudo lib/mtg.sh
+sudo ./lib/mtg.sh
 
 ## lookout for interuptions
-sudo lib/notice.sh
+sudo ./lib/notice.sh
