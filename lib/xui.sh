0419038e82c37b2acd43195300735e04537e7e45
damshenas <19164817+damshenas@users.noreply.github.com>
2024-10-15 13:08:49 +0200

    moved to usr local

--- lib/xui.sh
+++ lib/xui.sh
@@ -8,20 +8,22 @@ mkdir -p $cert_dir
 cp $s3_dir/sib.ftp.sh.cer $cert_dir/
 cp $s3_dir/sib.ftp.sh.key $cert_dir/
 
-cp $scripts_dir/lib/xui.service /etc/systemd/system/x-ui.service
+# cp $scripts_dir/lib/xui.service /etc/systemd/system/x-ui.service
 
 mkdir -p /etc/x-ui
 cp $s3_dir/x-ui.db /etc/x-ui/x-ui.db
 
-cd /opt
+cd /usr/local
 wget -q --timeout 10 --no-check-certificate -O x-ui-linux-${arch}.tar.gz https://github.com/MHSanaei/3x-ui/releases/download/${xui_version}/x-ui-linux-${arch}.tar.gz
 tar zxvf x-ui-linux-${arch}.tar.gz
 rm x-ui-linux-${arch}.tar.gz -f
 cd x-ui
 chmod +x x-ui
 chmod +x x-ui bin/xray-linux-${arch}
+cp -f x-ui.service /etc/systemd/system/
+
 wget -q --timeout 10 --no-check-certificate -O /usr/bin/x-ui https://raw.githubusercontent.com/MHSanaei/3x-ui/main/x-ui.sh
-chmod +x /opt/x-ui/x-ui.sh
+chmod +x /usr/local/x-ui/x-ui.sh
 chmod +x /usr/bin/x-ui
 
 systemctl daemon-reload
