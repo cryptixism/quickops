7210209561b972060f376d36531f43b051bff7c6
damshenas <19164817+damshenas@users.noreply.github.com>
2024-10-27 09:23:27 +0100

    added scale in notice and change sleep time

--- lib/notice.sh
+++ lib/notice.sh
@@ -1,18 +1,39 @@
 #!/bin/bash
 set +x
 
-TOKEN=`curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
+API_ADDR="http://169.254.169.254/latest"
+TOKEN_HEADER="NO_TOKEN"
 
-while sleep 5; do
+get_new_token () {
+  echo 'Getting new authentication token.' && date
+  TOKEN=`curl -s -X PUT "${API_ADDR}/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
+  TOKEN_HEADER="X-aws-ec2-metadata-token: ${TOKEN}"
+}
 
-  HTTP_CODE=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s -w %{http_code} -o /dev/null http://169.254.169.254/latest/meta-data/spot/instance-action)
+prepare_for_shutdown () {
+  echo 'Getting prepared for shutting down server.' && date
+  aws s3 sync ${s3_dir}/ s3://${s3_bucket_name}/data
+}
 
-  if [[ "$HTTP_CODE" -eq 401 ]] ; then
-    echo 'Refreshing Authentication Token'
-    TOKEN=`curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 30"`
-  elif [[ "$HTTP_CODE" -eq 200 ]] ; then
-    aws s3 sync $s3_dir/ s3://${s3_bucket_name}/data
-    echo "Synced. Ready for interuption." && date
+while sleep 2; do
+
+  ## spot interuption protection
+
+  HTTP_CODE=$(curl -s -H "${TOKEN_HEADER}" -s -w %{http_code} -o /dev/null "${API_ADDR}/meta-data/spot/instance-action")
+
+  if [[ "${HTTP_CODE}" -eq 401 ]] ; then
+    get_new_token
+  elif [[ "${HTTP_CODE}" -eq 200 ]] ; then
+    prepare_for_shutdown
+    break
+  fi
+
+  ## scale in protection
+
+  LIFECYCLE_STATE=$(curl -s -H "${TOKEN_HEADER}" "${API_ADDR}/meta-data/autoscaling/target-lifecycle-state")
+
+  if [[ "${LIFECYCLE_STATE}" == "Terminated" ]]; then
+    prepare_for_shutdown
     break
   fi
 
