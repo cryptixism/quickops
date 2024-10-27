38c188d2c1a30b553530e1ae39e60775f0cb818c
damshenas <19164817+damshenas@users.noreply.github.com>
2024-10-27 10:29:26 +0100

    added backup service

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,4 +18,7 @@ source $scripts_dir/lib/xui.sh
 source $scripts_dir/lib/mtg.sh
 
 ## lookout for interuptions
-source $scripts_dir/lib/notice.sh
+# source $scripts_dir/lib/notice.sh
+
+## backup service that trigger for interuptions, scale-in events, and terminations
+source $scripts_dir/lib/backup.sh
