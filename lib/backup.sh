77307ff14a084c2a594ff05f1746941c565d77ad
damshenas <19164817+damshenas@users.noreply.github.com>
2024-10-27 11:12:14 +0100

    fixed timer

--- lib/backup.sh
+++ lib/backup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 
 cp $scripts_dir/lib/backup.service /etc/systemd/system/backup.service
+cp $scripts_dir/lib/backup.timer /etc/systemd/system/backup.timer
 
 sed -i "s|BUCKET_NAME|${s3_bucket_name}|g" /etc/systemd/system/backup.service
 sed -i "s|S3_PATH|${s3_dir}|g" /etc/systemd/system/backup.service
