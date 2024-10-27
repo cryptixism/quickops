b1c5864794e7dd849d6d24133f9959a711319cf1
damshenas <19164817+damshenas@users.noreply.github.com>
2024-10-27 17:08:40 +0100

    services separated

--- lib/backup.sh
+++ lib/backup.sh
@@ -7,5 +7,9 @@ sed -i "s|BUCKET_NAME|${s3_bucket_name}|g" /etc/systemd/system/backup.service
 sed -i "s|S3_PATH|${s3_dir}|g" /etc/systemd/system/backup.service
 
 systemctl daemon-reload
-systemctl enable backup
-systemctl start backup
+
+systemctl enable backup.service
+systemctl enable backup.timer
+
+systemctl start backup.service
+systemctl start backup.timer
