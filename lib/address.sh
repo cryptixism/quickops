e10f335bf9a5f9cdecdf113125da96d5939f8753
damshenas <19164817+damshenas@users.noreply.github.com>
2024-10-27 18:40:38 +0100

    more readable

--- lib/address.sh
+++ lib/address.sh
@@ -4,7 +4,6 @@ export instance_id=$(cat /var/lib/cloud/data/instance-id)
 
 if [ $eip_allocation_id != "None" ]; then
   aws ec2 associate-address --instance-id ${instance_id} --allocation-id ${eip_allocation_id}
-else
+elif [ $afraid_update_key != "None" ]; then
   curl https://freedns.afraid.org/dynamic/update.php?${afraid_update_key}
 fi
-
