77f49d5feb96eac91978970b3ac7b537d8dfd412
damshenas <19164817+damshenas@users.noreply.github.com>
2024-10-28 20:58:09 +0100

    test2

--- lib/backup.sh
+++ lib/backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 cp $scripts_dir/lib/backup.service /etc/systemd/system/backup.service
-cp $scripts_dir/lib/backup.timer /etc/systemd/system/backup.timer
+# cp $scripts_dir/lib/backup.timer /etc/systemd/system/backup.timer
 
 sed -i "s|BUCKET_NAME|${s3_bucket_name}|g" /etc/systemd/system/backup.service
 sed -i "s|S3_PATH|${s3_dir}|g" /etc/systemd/system/backup.service
@@ -9,9 +9,9 @@ sed -i "s|S3_PATH|${s3_dir}|g" /etc/systemd/system/backup.service
 systemctl daemon-reload
 
 systemctl enable backup.service
-systemctl disable backup.timer
+# systemctl disable backup.timer
 
 systemctl start backup.service
-systemctl stop backup.timer
+# systemctl stop backup.timer
 
 # you can chek timer with: systemctl list-timers
