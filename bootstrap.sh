c9396403d3f0611e632c7a17ebf90da34a52ce68
damshenas <19164817+damshenas@users.noreply.github.com>
2024-10-28 22:28:00 +0100

    cleanup

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,8 +17,5 @@ source $scripts_dir/lib/xui.sh
 ## https://github.com/9seconds/mtg
 source $scripts_dir/lib/mtg.sh
 
-## lookout for interuptions and scale-in events
-# source $scripts_dir/lib/notice.sh
-
 ## backup service that trigger for interuptions, scale-in events, and terminations
 source $scripts_dir/lib/backup.sh
