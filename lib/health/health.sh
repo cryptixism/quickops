1e845d7be02d5a6f8151aaa7e02233e2d399a3ee
damshenas <19164817+damshenas@users.noreply.github.com>
2024-10-30 16:18:20 +0100

    refactored

--- lib/health/health.sh
+++ lib/health/health.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 
-PORTS=(8080 54321 5264)
-HOST="127.0.0.1"
-#SHUTDOWN_CMD="shutdown -h now"
-SHUTDOWN_CMD="echo $(date); echo '!!! Shutting Down'"
-
-# Function to check a single port
 check_port() {
     local port=$1
-    nc -z -w5 $HOST $port
+    nc -z -w5 127.0.0.1 $port
     return $?
 }
 
-# Check all ports
-for port in "${PORTS[@]}"; do
-    if ! check_port $port; then
-        echo "Port $port is closed or unreachable."
-        echo "Shutting down the server..."
-        $SHUTDOWN_CMD
-        exit 1
-    else
-        echo "Port $port is open."
-    fi
-done
+if ! check_port $mtg_port; then
+    echo "Port $mtg_port is closed or unreachable. Restarting mtg service..."
+    sudo systemctl restart mtg
+fi
+
+if ! check_port $xui_port || ! check_port $xray_port; then
+    echo "One of the xui or xray ports is closed or unreachable. Restarting xui service..."
+    sudo systemctl restart xui
+fi
