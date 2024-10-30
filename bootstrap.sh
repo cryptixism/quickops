e6e0393bbbdb427721552f5087eba9cd25d8c1fb
damshenas <19164817+damshenas@users.noreply.github.com>
2024-10-30 16:41:18 +0100

    changed order

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,16 +10,16 @@ mkdir -p $s3_dir
 ## handle address updates
 source $scripts_dir/lib/address/run.sh
 
-## installing and configuring xui and xray
-## https://github.com/MHSanaei/3x-ui
-source $scripts_dir/lib/xui/run.sh
-
 ## installing and configuring mtg
 ## https://github.com/9seconds/mtg
 source $scripts_dir/lib/mtg/run.sh
 
-## backup service that trigger for interuptions, scale-in events, and terminations
-source $scripts_dir/lib/backup/run.sh
+## installing and configuring xui and xray
+## https://github.com/MHSanaei/3x-ui
+source $scripts_dir/lib/xui/run.sh
 
 ## health service that checks health 
 source $scripts_dir/lib/health/run.sh
+
+## backup service that trigger for interuptions, scale-in events, and terminations
+source $scripts_dir/lib/backup/run.sh
