2979dfae289244c43795ab4bd8c0b6386fc0c58e
damshenas <19164817+damshenas@users.noreply.github.com>
2024-12-25 23:33:49 +0100

    working

--- lib/openvpn/run.sh
+++ lib/openvpn/run.sh
@@ -3,7 +3,7 @@
 # Update and install required packages
 apt install -y openvpn easy-rsa
 useradd user1
-echo "user1:${OPENVPN_USER1_PASSWORD}" | sudo chpasswd
+echo "user1:${openvpn_user1_password}" | sudo chpasswd
 
 # Set variables
 EASYRSA_DIR="/etc/openvpn/easy-rsa"
@@ -35,9 +35,12 @@ cp $EASYRSA_DIR/pki/issued/server.crt /etc/openvpn/
 cp $EASYRSA_DIR/pki/dh.pem /etc/openvpn/
 cp $KEY_DIR/ta.key /etc/openvpn/
 
+ca_cert=$(cat /etc/openvpn/ca.crt)
+
 # Replace variables in client config
 sed -i -e "s/SERVER_ADDRESS/${domain_name}/g" $CLIENT_CONFIG
 sed -i -e "s/SERVER_PORT/${openvpn_port}/g" $CLIENT_CONFIG
+sed -i -e "s/SERVER_CA/${ca_cert}/g" $CLIENT_CONFIG
 sed -i -e "s/SERVER_PORT/${openvpn_port}/g" $SERVER_CONFIG
 echo -e "\nCLient config:\n" && cat $CLIENT_CONFIG
 
