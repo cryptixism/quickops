d2d67bc701596d7c660bce4fc7f7f56d975bfa70
damshenas <19164817+damshenas@users.noreply.github.com>
2024-12-26 15:27:10 +0100

    reusing certs

--- lib/openvpn/run.sh
+++ lib/openvpn/run.sh
@@ -1,49 +1,39 @@
 #!/bin/bash
 
-# Update and install required packages
-apt install -y openvpn easy-rsa
+## Update and install required packages
+apt install -y openvpn 
 useradd user1
 echo "user1:${openvpn_user1_password}" | sudo chpasswd
 
-# Set variables
-EASYRSA_DIR="/etc/openvpn/easy-rsa"
-KEY_DIR="$EASYRSA_DIR/keys"
-CLIENT_DIR="/etc/openvpn/ccd"
-USER="user1"
-CLIENT_CONFIG="${CLIENT_DIR}/${USER}.ovpn"
-SERVER_CONFIG="/etc/openvpn/server.conf"
+## Set variables
+OPENVPN_DIR="/etc/openvpn/"
+CLIENT_CONFIG="$OPENVPN_DIR/user1.ovpn"
+SERVER_CONFIG="$OPENVPN_DIR/server.conf"
 
-# Configure easy-rsa
-make-cadir $EASYRSA_DIR
-mkdir -p $KEY_DIR $CLIENT_DIR
-
+## Copy configs
 cp $scripts_dir/lib/openvpn/client.ovpn $CLIENT_CONFIG
 cp $scripts_dir/lib/openvpn/server.conf $SERVER_CONFIG
 
-cd $EASYRSA_DIR
-./easyrsa init-pki
-echo -e "yes\n" | ./easyrsa build-ca nopass
-./easyrsa gen-dh
-echo -e "yes\n" | ./easyrsa build-server-full server nopass
-# ./easyrsa build-client-full $USER nopass
-openvpn --genkey secret $KEY_DIR/ta.key
+## Download certs
+aws s3api get-object --bucket "$s3_bucket_name" --key data/openvpn/ca.crt $OPENVPN_DIR/
+aws s3api get-object --bucket "$s3_bucket_name" --key data/openvpn/dh.pem $OPENVPN_DIR/
+aws s3api get-object --bucket "$s3_bucket_name" --key data/openvpn/server.crt $OPENVPN_DIR/
+aws s3api get-object --bucket "$s3_bucket_name" --key data/openvpn/server.key $OPENVPN_DIR/
+aws s3api get-object --bucket "$s3_bucket_name" --key data/openvpn/ta.key $OPENVPN_DIR/
 
-# Copy server keys and certificates
-cp $EASYRSA_DIR/pki/ca.crt /etc/openvpn/
-cp $EASYRSA_DIR/pki/private/server.key /etc/openvpn/
-cp $EASYRSA_DIR/pki/issued/server.crt /etc/openvpn/ 
-cp $EASYRSA_DIR/pki/dh.pem /etc/openvpn/
-cp $KEY_DIR/ta.key /etc/openvpn/
-
-ca_cert=$(cat /etc/openvpn/ca.crt)
-
-# Replace variables in client config
+## Replace variables in configs
+sed -i -e "s/SERVER_PORT/${openvpn_port}/g" $SERVER_CONFIG
 sed -i -e "s/SERVER_ADDRESS/${domain_name}/g" $CLIENT_CONFIG
 sed -i -e "s/SERVER_PORT/${openvpn_port}/g" $CLIENT_CONFIG
-sed -i -e "s/SERVER_CA/${ca_cert}/g" $CLIENT_CONFIG
-sed -i -e "s/SERVER_PORT/${openvpn_port}/g" $SERVER_CONFIG
-echo -e "\nCLient config:\n" && cat $CLIENT_CONFIG
+
+## Add CA certificate to client config
+echo -e "<ca>\n" >> $CLIENT_CONFIG
+cat ca.crt >> $CLIENT_CONFIG
+echo -e "</ca>\n" >> $CLIENT_CONFIG
 
 # Start OpenVPN service
 systemctl start openvpn@server
 systemctl enable openvpn@server
+
+# troubleshoot
+# journalctl -u openvpn@server -f
