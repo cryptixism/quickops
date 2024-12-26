e0d9e97a9a2b531e331cbab50f8a403392affd8f
damshenas <19164817+damshenas@users.noreply.github.com>
2024-12-26 11:17:40 +0100

    test2

--- lib/openvpn/run.sh
+++ lib/openvpn/run.sh
@@ -13,26 +13,31 @@ USER="user1"
 CLIENT_CONFIG="${CLIENT_DIR}/${USER}.ovpn"
 SERVER_CONFIG="/etc/openvpn/server.conf"
 
-# Configure easy-rsa
+## Configure easy-rsa
 make-cadir $EASYRSA_DIR
 mkdir -p $KEY_DIR $CLIENT_DIR
 
+## Certificates
 cp $scripts_dir/lib/openvpn/client.ovpn $CLIENT_CONFIG
 cp $scripts_dir/lib/openvpn/server.conf $SERVER_CONFIG
 cp $s3_dir/${domain_name}.cer /etc/openvpn/server.cer
 cp $s3_dir/${domain_name}.key /etc/openvpn/server.key
 
+## CA
+wget -q --timeout 10 --no-check-certificate -O isrgrootx1.pem https://letsencrypt.org/certs/isrgrootx1.pem
+cp isrgrootx1.pem /etc/openvpn/ca.pem
+ca_cert=$(cat /etc/openvpn/ca.pem)
+
+## DH 
 cd $EASYRSA_DIR
 ./easyrsa init-pki
 ./easyrsa gen-dh
-
 cp $EASYRSA_DIR/pki/dh.pem /etc/openvpn/
-
-ca_cert=$(cat /etc/openvpn/ca.crt)
 
 # Replace variables in client config
 sed -i -e "s/SERVER_ADDRESS/${domain_name}/g" $CLIENT_CONFIG
 sed -i -e "s/SERVER_PORT/${openvpn_port}/g" $CLIENT_CONFIG
+sed -i -e "s/CA_CONTENT/${ca_cert}/g" $CLIENT_CONFIG
 sed -i -e "s/SERVER_PORT/${openvpn_port}/g" $SERVER_CONFIG
 echo -e "\nCLient config:\n" && cat $CLIENT_CONFIG
 
