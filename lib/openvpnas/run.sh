4afc0f2ddc0f895010849b83d01b561f34b96419
damshenas <damshenas>
2025-07-12 07:51:18 +0200

    enable dco

--- lib/openvpnas/run.sh
+++ lib/openvpnas/run.sh
@@ -7,16 +7,23 @@ wget https://packages.openvpn.net/as-repo-public.asc -qO /etc/apt/keyrings/as-re
 echo "deb [arch=arm64 signed-by=/etc/apt/keyrings/as-repository.asc] http://packages.openvpn.net/as/debian jammy main" > /etc/apt/sources.list.d/openvpn-as-repo.list
 
 apt-get update -y
-apt-get install -y openvpn-as
+apt-get install -y openvpn-as build-essential gcc-12 g++-12
+git clone https://github.com/OpenVPN/ovpn-dco.git
+cd ovpn-dco
+make CC=gcc-12
+make install
+depmod -a
+modprobe ovpn-dco-v2
+
+systemctl enable openvpnas
+
+# restore configuration
 
 source $scripts_dir/lib/openvpnas/restore.sh
 
 chown openvpn_as:openvpn_as /usr/local/openvpn_as/etc/db/*.db /usr/local/openvpn_as/etc/as.conf
 
 systemctl restart openvpnas
-
-systemctl enable openvpnas
-systemctl start openvpnas
 
 # enable backup service
 
