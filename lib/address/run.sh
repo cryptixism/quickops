18259a730928f48ec52659e9d40d8ac0e8cda87b
damshenas <damshenas>
2025-07-12 06:32:43 +0200

    region

--- lib/address/run.sh
+++ lib/address/run.sh
@@ -4,7 +4,7 @@ export instance_id=$(cat /var/lib/cloud/data/instance-id)
 aws ec2 modify-instance-attribute --region ${aws_region} --instance-id ${instance_id} --no-source-dest-check
 
 if [ $eip_allocation_id != "None" ]; then
-  aws ec2 associate-address --instance-id ${instance_id} --allocation-id ${eip_allocation_id}
+  aws ec2 associate-address --region ${aws_region} --instance-id ${instance_id} --allocation-id ${eip_allocation_id}
 elif [ $afraid_update_key != "None" ]; then
   curl https://freedns.afraid.org/dynamic/update.php?${afraid_update_key}
 fi
